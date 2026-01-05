ba5a528c8ebe52eb2de9655e5cac32dcb61040a3
darshan09-h <darshan090704@gmail.com>
2026-01-05 17:22:03 +0530

    Add suspense wrapper in page.js

--- app/trip-status/page.js
+++ app/trip-status/page.js
@@ -1,7 +1,12 @@
+import { Suspense } from "react";
 import TripStatusClient from "./TripStatusClient";
 
 export default function TripStatusPage({ searchParams }) {
 //   const rideId = searchParams.rideId; // safe access
 
-  return <TripStatusClient />;
+ return (
+    <Suspense fallback={<div>Loading trip...</div>}>
+      <TripStatusClient />
+    </Suspense>
+  );
 }
